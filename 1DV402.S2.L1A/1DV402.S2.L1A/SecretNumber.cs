33ee730d380c8832656fd6947d4836d214d7d637
WictorKihlbaum <wk222as@student.lnu.se>
2014-09-30 17:16:49 +0200

    Gissa det hemliga talet
    En bit pa vag

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    class SecretNumber
+    {
+        public const int MaxNumberOfGuesses = 7;
+        private int _count;
+        private int _number;
+
+
+        public void Initialize()
+        {
+            _count = 0;
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100);
+
+            randomNumber = _number; 
+
+        }
+
+        public bool MakeGuess(int number)
+        {
+            if (number < _number)
+            {
+                Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, (7 - _count));
+            }
+
+            if (number > _number)
+            {
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, (7 - _count));
+            }
+
+            if (number == _number)
+            {
+                Console.WriteLine("RÄTT GISSAT. Du klarade det på {0} försök.", _count);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public SecretNumber()
+
+        { 
+        
+        }
+
+    }
+}
